d27514e40e9b84c1638af985c80834d5c365b9cc
moetarou <moexx0821@gmail.com>
2019-12-09 15:41:25 +0900

    add html,js,css

---
+++ css/script.js
@@ -0,0 +1,9 @@
+    
+    
+    
+    <script type="text/javascript">
+        $('.sp_menu').on('click',function(){
+            $('.menu__line').toggleClass('active');
+            $('.gnav').fadeToggle();
+        });
+    </script>
---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+<script type="text/javascript">
+    $('.sp_menu').on('click',function(){
+        $('.menu__line').toggleClass('active');
+        $('.gnav').fadeToggle();
+    });
+</script>
