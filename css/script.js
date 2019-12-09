0d3ecda8b2212e4376ca8f1a2fd097346642d98c
moetarou <moexx0821@gmail.com>
2019-12-09 15:43:47 +0900

    remove js from vss folfer,edited html

--- css/script.js
+++
@@ -1,9 +0,0 @@
-    
-    
-    
-    <script type="text/javascript">
-        $('.sp_menu').on('click',function(){
-            $('.menu__line').toggleClass('active');
-            $('.gnav').fadeToggle();
-        });
-    </script>
